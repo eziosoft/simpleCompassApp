ecda6e4c8db51c298e7543cb873a0240b2c11268
ezio <eziotest@gmail.com>
2021-03-14 22:35:20 +0100

    screenShot

--- app/src/androidTest/java/com/eziosoft/simplecompassnetguru/MainActivityTest.kt
+++ app/src/androidTest/java/com/eziosoft/simplecompassnetguru/MainActivityTest.kt
@@ -1,19 +1,28 @@
 package com.eziosoft.simplecompassnetguru
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.filters.SmallTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.eziosoft.simplecompassnetguru.ui.MainActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
-@RunWith(AndroidJUnit4ClassRunner::class)
+@RunWith(AndroidJUnit4::class)
+@MediumTest
 class MainActivityTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun test_isActivityInView() {
